45fdeafadda0d6215420c4600c3c60d58ad7e783
= <=>
2015-10-20 22:51:00 -0600

    Use an Iterator

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,14 @@ extern crate id3;
 use id3::Tag;
 use std::string;
 use std::env;
-use std::error;
-use std::fs::{self,DirEntry,File};
+use std::fs::{self,File};
 use std::io::{self,Read};
-use std::path::{Path,PathBuf};
+use std::path::PathBuf;
 
 #[derive(Debug)]
 enum Mp3Error {
     IoError(io::Error),
     Mp3StringError(string::FromUtf8Error),
-    InvalidMp3Error(String),
 }
 
 impl From<io::Error> for Mp3Error {
@@ -38,9 +36,8 @@ fn is_mp3(file: &PathBuf) -> Result<bool, Mp3Error> {
         }
         let result = match String::from_utf8(Vec::from(&buffer[0..3])) {
             Ok(r) => r,
-            Err(e) => return Ok(false),
+            Err(_) => return Ok(false),
         };
-        // let result = try!(String::from_utf8(Vec::from(&buffer[0..3])));
         Ok(result == "ID3")
     } else {
         Ok(false)
@@ -48,49 +45,108 @@ fn is_mp3(file: &PathBuf) -> Result<bool, Mp3Error> {
 }
 
 
-fn walk_dir(root: PathBuf) -> Result<Vec<PathBuf>, Mp3Error> {
-    // recursively walk the directory, looking for all paths
-    // that look like mp3s and shoving them into a Vec.
-    // this is only really necessary because walk_dir is unstable
-    // and compiling rust sucks
-    let mut vec: Vec<PathBuf> = Vec::new();
-    if try!(fs::metadata(&root)).is_dir() {
-        for entry in try!(fs::read_dir(root)) {
-            // if it's a directory, recurse.
-            // let entry = try!(entry);
-            let path = try!(entry).path();
-            let metadata = try!(fs::metadata(&path));
-            if metadata.is_dir() {
-                vec.extend(try!(walk_dir(path)));
-            } else if metadata.is_file() {
-                if try!(is_mp3(&path)) {
-                    vec.push(path);
-                }
-            // } else { //metadata.is_symlink(), I guess?
-                // TODO: how should I handle symlinks?
-            }
+struct DirectoryWalker {
+    directories: Vec<PathBuf>,
+    files: Vec<PathBuf>,
+    errors: Vec<Mp3Error>
+}
+
+impl DirectoryWalker {
+    fn new(root: PathBuf) -> DirectoryWalker {
+        let mut directories: Vec<PathBuf> = Vec::new();
+        directories.push(root);
+        DirectoryWalker {
+            directories: directories,
+            files: Vec::new(),
+            errors: Vec::new(),
         }
     }
-    Ok(vec)
+    fn handle_entry(&mut self, path: PathBuf) -> Option<PathBuf> {
+
+        let metadata = match fs::metadata(&path) {
+            Ok(m) => m,
+            Err(e) => {
+                self.errors.push(Mp3Error::from(e));
+                return None
+            },
+        };
+        if metadata.is_dir() {
+            self.directories.push(path);
+            None
+        } else if metadata.is_file() && is_mp3(&path).unwrap_or(false) {
+            Some(path)
+        } else {
+            None
+        }
+    }
+    fn repopulate_files(&mut self, dirpath: PathBuf) {
+        let readdir = match fs::read_dir(dirpath) {
+            Err(e) => {
+                self.errors.push(Mp3Error::from(e));
+                return
+            },
+            Ok(r) => r
+        };
+        for entry in readdir {
+            match entry {
+                Err(e) => self.errors.push(Mp3Error::from(e)),
+                Ok(f) => self.files.push(f.path()),
+            }
+        };
+    }
+}
+
+impl Iterator for DirectoryWalker {
+    type Item = PathBuf;
+    fn next(&mut self) -> Option<PathBuf> {
+        loop {
+            while !self.files.is_empty() {
+                let path = match self.files.pop() {
+                    Some(path) => path,
+                    None => continue,
+                };
+                // pushes new things into directories/errors, only returns
+                // something when it finds something (or None to try the next file)
+                match self.handle_entry(path) {
+                    Some(path) => return Some(path),
+                    None => continue,
+                }
+            }
+            // done with the current file list. Let's repopulate it.
+            let dirpath = match self.directories.pop() {
+                Some(dirpath) => dirpath,
+                None => return None,
+            };
+            self.repopulate_files(dirpath);
+        }
+    }
+}
+
+
+fn print_tag(mp3: PathBuf) {
+    let tag = Tag::read_from_path(mp3).unwrap();
+    println!("{} - {} - {}",
+        tag.artist().unwrap_or("???"),
+        tag.album().unwrap_or("???"),
+        tag.title().unwrap_or("???")
+    );
 }
 
 
 fn main() {
-    // let mut tag = Tag::read_from_path("sample.mp3").unwrap();
     let args: Vec<String> = env::args().skip(1).collect();
     for arg in args {
-        let mp3s: Vec<PathBuf> = match walk_dir(PathBuf::from(&arg)) {
-            Ok(paths) => paths,
-            Err(e) => panic!("File {} failed: {:?}", arg, e),
-        };
-        for mp3 in mp3s {
-            let mut tag = Tag::read_from_path(mp3).unwrap();
-            println!("{} - {} - {}",
-                tag.artist().unwrap_or("???"),
-                tag.album().unwrap_or("???"),
-                tag.title().unwrap_or("???")
-            );
-
+        let mut mp3s = DirectoryWalker::new(PathBuf::from(&arg));
+        // can't just loop over it with for, because the borrow checker gets all
+        // upset about me using it later to access the errors.
+        loop {
+            match mp3s.next() {
+                Some(mp3) => print_tag(mp3),
+                None => break,
+            }
+        }
+        for error in mp3s.errors {
+            println!("error: {:?}", error);
         }
     }
 }
